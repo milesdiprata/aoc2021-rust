446abe514432e6f9a28f7c455c156ca42b5059f7
Miles di Prata <milesdiprata3@gmail.com>
2022-06-23 17:29:14 -0400

    Base Day 19 solution without rotation in 2D.

--- src/day19.rs
+++ src/day19.rs
@@ -1,16 +1,14 @@
 use core::fmt;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io::{self, BufRead};
-use std::ops::{Add, Sub};
+use std::iter::Sum;
+use std::ops::{Add, AddAssign, Sub, SubAssign};
 use std::str::FromStr;
 
 use anyhow::{anyhow, Error, Result};
 
-const SENSOR_MAX_RNG: isize = 5;
-const MIN_COMMON_BEACONS: usize = 3;
-
 #[derive(PartialEq, Eq, Hash, Clone)]
-struct Beacon {
+struct Point {
     x: isize,
     y: isize,
 }
@@ -18,16 +16,16 @@ struct Beacon {
 #[derive(Debug)]
 struct Scanner {
     id: usize,
-    beacons: Vec<Beacon>,
+    beacons: Vec<Point>,
 }
 
-impl fmt::Debug for Beacon {
+impl fmt::Debug for Point {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(fmt, "({},{})", self.x, self.y)
     }
 }
 
-impl FromStr for Beacon {
+impl FromStr for Point {
     type Err = Error;
 
     fn from_str(str: &str) -> Result<Self> {
@@ -44,33 +42,65 @@ impl FromStr for Beacon {
                 .parse()?,
         );
 
-        Ok(Beacon { x, y })
+        Ok(Point { x, y })
     }
 }
 
-impl<'a, 'b> Add<&'b Beacon> for &'a Beacon {
-    type Output = Beacon;
+impl Default for Point {
+    fn default() -> Self {
+        Self {
+            x: Default::default(),
+            y: Default::default(),
+        }
+    }
+}
 
-    fn add(self, other: &'b Beacon) -> Beacon {
-        Beacon {
+impl<'a, 'b> Add<&'b Point> for &'a Point {
+    type Output = Point;
+
+    fn add(self, other: &'b Point) -> Point {
+        Point {
             x: self.x + other.x,
             y: self.y + other.y,
         }
     }
 }
 
-impl<'a, 'b> Sub<&'b Beacon> for &'a Beacon {
-    type Output = Beacon;
+impl<'a, 'b> Sub<&'b Point> for &'a Point {
+    type Output = Point;
 
-    fn sub(self, other: &'b Beacon) -> Beacon {
-        Beacon {
+    fn sub(self, other: &'b Point) -> Point {
+        Point {
             x: self.x - other.x,
             y: self.y - other.y,
         }
     }
 }
 
+impl<'a> AddAssign<&'a Point> for Point {
+    fn add_assign(&mut self, other: &'a Self) {
+        self.x += other.x;
+        self.y += other.y;
+    }
+}
+
+impl<'a> SubAssign<&'a Point> for Point {
+    fn sub_assign(&mut self, other: &'a Self) {
+        self.x -= other.x;
+        self.y -= other.y;
+    }
+}
+
+impl<'a> Sum<&'a Self> for Point {
+    fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(Self::default(), |acc, point| &acc + point)
+    }
+}
+
 impl Scanner {
+    const MAX_RNG: isize = 5;
+    const MIN_COMMON_BEACONS: usize = 3;
+
     fn from_str_slice(slice: &[&str]) -> Result<Self> {
         let mut lines = slice.iter();
 
@@ -89,6 +119,32 @@ impl Scanner {
             true => Err(anyhow!("Empty scanner readings!")),
             false => Ok(Self { id, beacons }),
         }
+    }
+
+    fn find_min_common_beacons(
+        &self,
+        other: &Self,
+    ) -> Option<HashMap<usize, HashMap<usize, HashMap<Point, Point>>>> {
+        let beacon_pairs = self
+            .beacons
+            .iter()
+            .flat_map(|i| other.beacons.iter().map(move |j| (i, j)))
+            .map(|(i, j)| (i.clone(), j.clone()))
+            .collect::<Vec<_>>();
+
+        (-Self::MAX_RNG..=Self::MAX_RNG)
+            .flat_map(|x| (-Self::MAX_RNG..=Self::MAX_RNG).map(move |y| (x, y)))
+            .map(|(x, y)| Point { x, y })
+            .map(|offset| {
+                beacon_pairs
+                    .iter()
+                    .filter(|(i, j)| &(i + &offset) == j)
+                    .cloned()
+                    .collect::<HashMap<_, _>>()
+            })
+            .find(|common| common.len() >= Self::MIN_COMMON_BEACONS)
+            .map(|common| HashMap::from([(other.id, common)]))
+            .map(|common| HashMap::from([(self.id, common)]))
     }
 }
 
@@ -144,28 +200,7 @@ fn read_scanners() -> Result<Vec<Scanner>> {
     Ok(scanners)
 }
 
-fn find_min_common_beacons(i: &Scanner, j: &Scanner) -> Option<HashMap<Beacon, Beacon>> {
-    let beacon_pairs = i
-        .beacons
-        .iter()
-        .flat_map(|i| j.beacons.iter().map(move |j| (i, j)))
-        .map(|(i, j)| (i.clone(), j.clone()))
-        .collect::<Vec<_>>();
-
-    (-SENSOR_MAX_RNG..=SENSOR_MAX_RNG)
-        .flat_map(|x| (-SENSOR_MAX_RNG..=SENSOR_MAX_RNG).map(move |y| (x, y)))
-        .map(|(x, y)| Beacon { x, y })
-        .map(|offset| {
-            beacon_pairs
-                .iter()
-                .filter(|(i, j)| &(i + &offset) == j)
-                .map(|(i, j)| (i.clone(), j.clone()))
-                .collect::<HashMap<_, _>>()
-        })
-        .find(|common| common.len() >= MIN_COMMON_BEACONS)
-}
-
-fn part_one(scanners: &[Scanner]) -> () {
+fn part_one(scanners: &[Scanner]) -> usize {
     // Compute all possible pairs of scanners
     let scanner_pairs = scanners
         .iter()
@@ -174,33 +209,67 @@ fn part_one(scanners: &[Scanner]) -> () {
         .collect::<Vec<_>>();
 
     // Find pairs of scanners that have 12 common beacons
+    // TODO: Account for rotations about Euclidean space
     let scanner_common_beacons = scanner_pairs
         .iter()
-        .map(|&(i, j)| (i, j, find_min_common_beacons(i, j)))
-        .filter(|(_, _, common)| common.is_some())
-        .map(|(i, j, common)| (i.id, j.id, common.unwrap()))
-        .map(|(i, j, common)| (i, HashMap::from([(j, common)])))
+        .flat_map(|&(i, j)| i.find_min_common_beacons(j))
+        .fold(HashMap::new(), |acc, common| {
+            acc.into_iter().chain(common.into_iter()).collect()
+        });
+
+    // Determine relative sensor positions to each other
+    let scanner_rel_pos = scanner_common_beacons
+        .iter()
+        .map(|(&id, common)| {
+            (
+                id,
+                common
+                    .iter()
+                    .map(|(&id, common)| (id, common.iter().next().map(|(i, j)| i - j).unwrap()))
+                    .collect::<HashMap<_, _>>(),
+            )
+        })
         .collect::<HashMap<_, _>>();
 
-    // Reconstruct beacon map one sensor at a time
-    let scanner_offsets = todo!();
-    todo!();
+    // Determine 'absolute' sensor positions to 0th sensor
+    let scanner_abs_pos = scanner_rel_pos
+        .iter()
+        .filter(|(&id, _)| id != 0)
+        .map(|(&id, common)| (id, common.values().sum::<Point>()))
+        .collect::<HashMap<_, _>>();
 
-    // Express absolute beacon positions relative to scanner 0 with its orientation and coords at (0,0,0)
-    todo!();
+    println!("abs {:?}", scanner_abs_pos);
 
-    todo!()
+    // Determine 'absolute' beacon positions to 0th sensor
+    let mut scanners = scanners.iter();
+
+    let beacons = scanners
+        .next()
+        .unwrap()
+        .beacons
+        .iter()
+        .cloned()
+        .chain(
+            scanners
+                .map(|scanner| {
+                    scanner
+                        .beacons
+                        .iter()
+                        .map(|beacon| beacon - &scanner_abs_pos[&scanner.id])
+                })
+                .flatten(),
+        )
+        .collect::<HashSet<_>>();
+
+    println!("beacons {:?}", beacons);
+
+    beacons.len()
 }
 
 fn main() -> Result<()> {
     let mut scanners = read_scanners()?;
 
-    // let pairs = scanners
-    //     .iter()
-    //     .flat_map(|i| scanners.iter().map(move |j| (i, j)))
-    //     .collect::<Vec<_>>();
-
-    part_one(&scanners);
+    println!("Part one: {}", part_one(&scanners));
 
     Ok(())
 }
